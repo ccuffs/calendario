7c1a640434ec9823c0ad86eee45a22db23188771
Fernando Bevilacqua <dovyski@gmail.com>
2019-09-04 11:53:04 -0300

    Re-factoring do JS para ficar mais modular

--- js/app.js
+++ js/app.js
@@ -6,20 +6,43 @@
  */
 
 var CALENDAR = new function() {
-    this.render = function(results) {
+    const CALENDAR_FILE = 'calendario-2019-02.tsv';
+    const DATA_URL = './data/2019/' + CALENDAR_FILE + '?nocache=' + Math.random();
+
+    this.rawData = null;
+    this.header = [];
+    this.data = [];
+
+    this.render = function() {
         var aItemTemplate = $('#month-item');
-        console.log(results);
+
+        console.log(this.data);
+        console.log(this.header);
+    };
+
+    this.organizeData = function(results) {
+        this.rawData = results.data.shi;
+        this.header = results.data.shift();
+        this.data = results.data;
+    };
+
+    this.load = function() {
+        Papa.parse(DATA_URL, {
+            download: true,
+            header: false,
+            complete: function(results) {
+                CALENDAR.start(results);
+            }
+        });
+    };
+
+    this.start = function(results) {
+        this.organizeData(results);
+        this.render();
     };
 
     this.init = function() {
-        var nocache = '?nocache=' + Math.random();
-        Papa.parse('./data/2019/calendario-2019-02.tsv' + nocache, {
-            download: true,
-            header: true,
-            complete: function(results) {
-                CALENDAR.render(results);
-            }
-        });
+        this.load();
     };
 };
 
